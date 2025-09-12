c08ec0499452d5f3bd278a22bec1579db3decf4d
Denis Salnikov <denissalnikov@ukr.net>
2025-09-13 02:58:19 +0300

    Fixing and testing DB

--- db/database.py
+++ db/database.py
@@ -2,13 +2,10 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncAttrs, async_sessio
 from sqlalchemy.orm import DeclarativeBase
 from core.config import settings
 
+engine = create_async_engine(settings.database_url, echo=settings.debug)
+
+async_session = async_sessionmaker(engine, expire_on_commit=False)
+
 
 class Base(AsyncAttrs, DeclarativeBase):
-    engine = create_async_engine(settings.database_url, echo=settings.debug)
-
-    async_session = async_sessionmaker(engine, expire_on_commit=False)
-
-    async def save(self):
-        async with self.async_session() as session:
-            session.add(self)
-            await session.commit()
+    pass
--- db/models.py
+++ db/models.py
@@ -9,7 +9,7 @@ class User(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     login: Mapped[str] = mapped_column(String(255))
-    registration_date: Mapped[datetime.date] = mapped_column(Date, server_default=func.now())
+    registration_date: Mapped[datetime.date] = mapped_column(Date, server_default=func.current_date())
 
     credits: Mapped[list["Credit"]] = relationship(back_populates='user', cascade="all, delete-orphan", lazy="joined")
 
@@ -18,14 +18,14 @@ class Credit(Base):
     __tablename__ = "credits"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
-    issuance_date: Mapped[datetime.date] = mapped_column(Date, server_default=func.now())
+    issuance_date: Mapped[datetime.date] = mapped_column(Date, server_default=func.current_date())
     return_date: Mapped[datetime.date] = mapped_column(Date)
     actual_return_date: Mapped[datetime.date] = mapped_column(Date)
     body: Mapped[int]
     percent: Mapped[float] = mapped_column(Numeric(3, 2))
 
     user: Mapped[User] = relationship(back_populates="credits", lazy="joined")
-    payments: Mapped[list["Payment"]] = relationship(back_populates="payment", cascade="all, delete-orphan", lazy="joined")
+    payments: Mapped[list["Payment"]] = relationship(back_populates="credit", cascade="all, delete-orphan", lazy="joined")
 
 
 class Term(Base):
@@ -38,9 +38,9 @@ class Payment(Base):
     __tablename__ = "payments"
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     sum: Mapped[int]
-    payment_date: Mapped[datetime.date] = mapped_column(Date, server_default=func.now())
-    credit_id: Mapped[Credit] = mapped_column(ForeignKey("credits.id"))
-    type_id: Mapped[Term] = mapped_column(ForeignKey("dictionary.id"))
+    payment_date: Mapped[datetime.date] = mapped_column(Date, server_default=func.current_date())
+    credit_id: Mapped[int] = mapped_column(ForeignKey("credits.id"))
+    type_id: Mapped[int] = mapped_column(ForeignKey("dictionary.id"))
 
     credit: Mapped[Credit] = relationship(back_populates="payments", lazy="joined")
     type: Mapped[Term] = relationship(lazy="joined")
